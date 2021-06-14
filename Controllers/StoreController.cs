fa5cfaa792ed8f9a3be346e639aa1da7eadfcac6
Theresa Dinh <hanhtheresadinh@gmail.com>
2021-06-13 21:08:04 -0700

    Added StoreController and made link to show product details

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CooliosoteaFinal.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(string product)
+        {
+            //Store selected product name in ViewBag to the view that has corresponding drink
+            ViewBag.product = product;
+            return View();
+        }
+    }
+}
