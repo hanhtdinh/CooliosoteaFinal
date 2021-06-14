38f5093d1ae9371ccf2a56956d16de4a0b48899f
Theresa Dinh <hanhtheresadinh@gmail.com>
2021-06-14 01:16:39 -0700

    Adding Identity into project and allowing creation of accounts

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CooliosoteaFinal.Models;
 
 namespace CooliosoteaFinal
 {
@@ -27,11 +28,23 @@ namespace CooliosoteaFinal
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<CooliosoteaContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            //configure identity to work w/ db
+            services.AddIdentity<ApplicationUser, ApplicationRole>()
+                .AddDefaultUI()
+
+                // use app role class to managed roles and permissions, route identity to existing CooliosoteaContext class
+                //use default cookie settings 
+
+                .AddRoles<ApplicationRole>()
+                .AddRoleManager<RoleManager<ApplicationRole>>()
+                .AddEntityFrameworkStores<CooliosoteaContext>()
+                .AddDefaultTokenProviders();
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //    .AddEntityFrameworkStores<CooliosoteaContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
